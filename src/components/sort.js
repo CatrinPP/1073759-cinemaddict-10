747a92402c2aea0bf4187953c27a6b6b47bde675
CatrinPP <caterina.parshina@gmail.com>
2020-01-07 01:56:37 +0300

    Добавляет виды сортировки

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,12 +1,22 @@
 import AbstractComponent from './abstract-component.js';
 
+export const SortType = {
+  DEFAULT: `default`,
+  DATE: `date`,
+  RATING: `rating`
+};
+
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+    this._currentSortType = SortType.DEFAULT;
+  }
   getTemplate() {
     return (
       `<ul class="sort">
-        <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-        <li><a href="#" class="sort__button">Sort by date</a></li>
-        <li><a href="#" class="sort__button">Sort by rating</a></li>
+        <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
+        <li><a href="#" data-sort-type="${SortType.DATE}" class="sort__button">Sort by date</a></li>
+        <li><a href="#" data-sort-type="${SortType.RATING}" class="sort__button">Sort by rating</a></li>
       </ul>`
     );
   }
