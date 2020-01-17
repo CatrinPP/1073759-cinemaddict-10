ac5859a440237b5adc6b478dfd6479586037bf1d
CatrinPP <caterina.parshina@gmail.com>
2020-01-17 17:30:52 +0300

    Удаляет recoveryListeners

--- src/components/abstract-smart-component.js
+++ src/components/abstract-smart-component.js
@@ -1,10 +1,6 @@
 import AbstractComponent from './abstract-component';
 
 export default class AbstractSmartComponent extends AbstractComponent {
-  recoveryListeners() {
-    throw new Error(`Abstract method not implemented: recoveryListeners`);
-  }
-
   rerender() {
     const oldElement = this.getElement();
     const parent = oldElement.parentElement;
@@ -13,7 +9,5 @@ export default class AbstractSmartComponent extends AbstractComponent {
 
     const newElement = this.getElement();
     parent.replaceChild(newElement, oldElement);
-
-    this.recoveryListeners();
   }
 }
