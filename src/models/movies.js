32805b39dfa0ff72a2258ee221aa703a2698619b
CatrinPP <caterina.parshina@gmail.com>
2020-01-20 16:01:25 +0300

    Добавляет в модель методы получения и записи фильмов

--- src/models/movies.js
+++ src/models/movies.js
@@ -2,4 +2,12 @@ export default class Movie {
   constructor() {
     this._cards = [];
   }
+
+  getMovies() {
+    return this._cards;
+  }
+
+  setMovies(cards) {
+    this._cards = Array.from(cards);
+  }
 }
