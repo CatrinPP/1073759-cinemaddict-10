0b0b262cbae6131936bd02dc36489a9d9512b491
CatrinPP <caterina.parshina@gmail.com>
2020-01-20 17:00:41 +0300

    Добавляет возможность обновления фильма

--- src/models/movies.js
+++ src/models/movies.js
@@ -10,4 +10,16 @@ export default class Movie {
   setMovies(cards) {
     this._cards = Array.from(cards);
   }
+
+  updateMovie(id, card) {
+    const index = this._cards.findIndex((it) => it.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    this._cards.splice(index, 1, card);
+
+    return true;
+  }
 }
