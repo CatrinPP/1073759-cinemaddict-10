883db1e2cd736603a1ab2d203e5f4e96e9c01be5
CatrinPP <caterina.parshina@gmail.com>
2020-01-24 18:49:11 +0300

    Создает компонент комментария

--- src/components/popup.js
+++ src/components/popup.js
@@ -21,35 +21,35 @@ const createGenresMarkup = (filmGenres) => {
  * @param  {object} comments объект структуры комментария
  * @return {string} html-код комментария
  */
-const createComment = (comments) => {
-  const {author, text, emoji, date} = comments;
+// const createComment = (comments) => {
+//   const {author, text, emoji, date} = comments;
 
-  return (
-    `<li class="film-details__comment">
-      <span class="film-details__comment-emoji">
-        <img src="${emoji}" width="55" height="55" alt="emoji">
-      </span>
-      <div>
-        <p class="film-details__comment-text">${text}</p>
-        <p class="film-details__comment-info">
-          <span class="film-details__comment-author">${author}</span>
-          <span class="film-details__comment-day">${moment(date).format(`YYYY/MM/DD HH:MM`)}</span>
-          <button class="film-details__comment-delete">Delete</button>
-        </p>
-      </div>
-    </li>`
-  );
-};
+//   return (
+//     `<li class="film-details__comment">
+//       <span class="film-details__comment-emoji">
+//         <img src="${emoji}" width="55" height="55" alt="emoji">
+//       </span>
+//       <div>
+//         <p class="film-details__comment-text">${text}</p>
+//         <p class="film-details__comment-info">
+//           <span class="film-details__comment-author">${author}</span>
+//           <span class="film-details__comment-day">${moment(date).format(`YYYY/MM/DD HH:MM`)}</span>
+//           <button class="film-details__comment-delete">Delete</button>
+//         </p>
+//       </div>
+//     </li>`
+//   );
+// };
 
 /**
    * Генерирует разметку со всеми комментарими
    * @param {array} comments массив с комментариями
    * @return {string} html-код со всеми комментариями
    */
-const generateComments = (comments) => {
-  const filmComments = comments.map((comm) => createComment(comm));
-  return filmComments;
-};
+// const generateComments = (comments) => {
+//   const filmComments = comments.map((comm) => createComment(comm));
+//   return filmComments;
+// };
 
 const createRatingMarkup = (card) => {
   const {poster, title} = card;
@@ -194,7 +194,7 @@ export default class Popup extends AbstractSmartComponent {
             <section class="film-details__comments-wrap">
               <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
 
-              <ul class="film-details__comments-list">${generateComments(comments)}</ul>
+              <ul class="film-details__comments-list"></ul>
 
               <div class="film-details__new-comment">
                 <div for="add-emoji" class="film-details__add-emoji-label"></div>
