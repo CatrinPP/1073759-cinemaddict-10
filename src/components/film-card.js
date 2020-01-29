2237c6770df061371e9a8566762d44000d3ac2d8
CatrinPP <caterina.parshina@gmail.com>
2020-01-30 00:10:17 +0300

    Исправляет представление жанров в карточке

--- src/components/film-card.js
+++ src/components/film-card.js
@@ -20,7 +20,7 @@ export default class FilmCard extends AbstractComponent {
         <p class="film-card__info">
           <span class="film-card__year">${year}</span>
           <span class="film-card__duration">${duration}</span>
-          <span class="film-card__genre">${genres[0]}</span>
+          <span class="film-card__genre">${genres.length ? genres[0] : ``}</span>
         </p>
         <img src=${poster} alt="" class="film-card__poster">
         <p class="film-card__description">${shortDescription}</p>
